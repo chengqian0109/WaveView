2902e425be10dbc4228a85c7772cf21135fb9789
chengqian <chengqian0109@126.com>
2020-11-18 14:26:47 +0800

    继承重构

--- WaveViewLibrary/src/main/java/com/jack/widget/WaveView.java
+++ WaveViewLibrary/src/main/java/com/jack/widget/WaveView.java
@@ -5,30 +5,34 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
-import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 /**
- * 乐动
+ * 乐动，线条默认为白色，相关自定义属性如下：<br/>
+ * WaveView_waveColor 线条颜色<br/>
+ * WaveView_waveCount 线条数量<br/>
+ * WaveView_waveWidth 线条宽度<br/>
+ * WaveView_waveMargin 相邻线条之间的间距<br/>
+ * WaveView_waveAnimDuration 线条动画执行时长<br/>
+ * WaveView_waveAnimDelay 相邻线条之间动画延时
  *
  * @author chengqian
  * Created on 2020/11/16
  */
-public class WaveView extends LinearLayout {
+public class WaveView extends View {
 
-    private View[] mChildViews;
+    private Paint mPaint;
 
-    private ValueAnimator[] mValueAnimators;
+    private int mWaveCount;
 
     /**
-     * 子 view 的 scaleY 的变化值
+     * 线条长度的变化比例
      */
     private float[] mFractions;
 
@@ -52,13 +56,16 @@ public class WaveView extends LinearLayout {
      */
     private final int DEFAULT_HEIGHT = dp2px(10);
 
-    private int mChildWidth = dp2px(1f);
+    private int mWaveWidth = dp2px(1f);
 
-    private int mChildHeight = DEFAULT_HEIGHT;
+    private int mWaveMargin = dp2px(1.5f);
 
-    private int mChildMargin = dp2px(1.5f);
+    private ValueAnimator[] mValueAnimators;
 
-    private int mColor;
+    /**
+     * 控件最终的高度
+     */
+    private int mHeight;
 
     public WaveView(Context context) {
         this(context, null);
@@ -66,53 +73,29 @@ public class WaveView extends LinearLayout {
 
     public WaveView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        // 此行代码用于子 View 的动态展现；另解，删除此行代码，设置控件背景
-        setWillNotDraw(false);
         init(context, attrs);
     }
 
     private void init(Context context, AttributeSet attrs) {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.WaveView);
-        mColor = typedArray.getColor(R.styleable.WaveView_waveColor, Color.WHITE);
-        int count = typedArray.getInt(R.styleable.WaveView_waveCount, 3);
-        int waveWidth = typedArray.getDimensionPixelSize(R.styleable.WaveView_waveWidth, mChildWidth);
-        mChildWidth = Math.max(waveWidth, mChildWidth);
-        int waveMargin = typedArray.getDimensionPixelSize(R.styleable.WaveView_waveMargin, mChildMargin);
-        mChildMargin = Math.max(waveMargin, mChildMargin);
+        int waveColor = typedArray.getColor(R.styleable.WaveView_waveColor, Color.WHITE);
+        mWaveCount = typedArray.getInt(R.styleable.WaveView_waveCount, 3);
+        int waveWidth = typedArray.getDimensionPixelSize(R.styleable.WaveView_waveWidth, mWaveWidth);
+        mWaveWidth = Math.max(waveWidth, mWaveWidth);
+        int waveMargin = typedArray.getDimensionPixelSize(R.styleable.WaveView_waveMargin, mWaveMargin);
+        mWaveMargin = Math.max(waveMargin, mWaveMargin);
         mAnimDuration = typedArray.getInteger(R.styleable.WaveView_waveAnimDuration, 240);
         mAnimDelay = typedArray.getInteger(R.styleable.WaveView_waveAnimDelay, 100);
         typedArray.recycle();
 
-        setOrientation(LinearLayout.HORIZONTAL);
-        setGravity(Gravity.CENTER_VERTICAL);
+        mPaint = new Paint();
+        mPaint.setColor(waveColor);
+        mPaint.setStrokeWidth(8);
+        mPaint.setStyle(Paint.Style.FILL);
 
-        mChildViews = new View[count];
-        mValueAnimators = new ValueAnimator[count];
-        mFractions = new float[count];
-        addChildren();
+        mValueAnimators = new ValueAnimator[mWaveCount];
+        mFractions = new float[mWaveCount];
         initAnim();
-    }
-
-    private void addChildren() {
-        for (int i = 0; i < mChildViews.length; i++) {
-            View view = new View(getContext());
-            view.setBackgroundColor(mColor);
-            LayoutParams params = new LayoutParams(mChildWidth, mChildHeight);
-            if (i != 0) {
-                params.setMarginStart(mChildMargin);
-            }
-            mChildViews[i] = view;
-            addView(view, params);
-        }
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        for (int i = 0; i < mChildViews.length; i++) {
-            mChildViews[i].setScaleY(mFractions[i]);
-        }
-        invalidate();
     }
 
     /**
@@ -120,7 +103,7 @@ public class WaveView extends LinearLayout {
      */
     public void initAnim() {
         for (int i = 0; i < mValueAnimators.length; i++) {
-            ValueAnimator animator = ValueAnimator.ofFloat(0.2f, 1);
+            ValueAnimator animator = ValueAnimator.ofFloat(0.3f, 1);
             animator.setDuration(mAnimDuration);
             animator.setInterpolator(new AccelerateDecelerateInterpolator());
             animator.setRepeatCount(ValueAnimator.INFINITE);
@@ -137,6 +120,72 @@ public class WaveView extends LinearLayout {
             });
             mValueAnimators[i] = animator;
             animator.start();
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mHeight = h;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.translate(0, mHeight / 2f);
+        for (int i = 0; i < mWaveCount; i++) {
+            canvas.drawRect(i * (mWaveWidth + mWaveMargin), -mFractions[i] * mHeight / 2,
+                    i * (mWaveWidth + mWaveMargin) + mWaveWidth, mFractions[i] * mHeight / 2, mPaint);
+        }
+        invalidate();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        // 计算所需的宽度，忽略计算宽度很大，比方说屏幕宽度不足以完全显示控件的情况
+        int widthSize = mWaveCount * mWaveWidth + (mWaveCount - 1) * mWaveMargin
+                + getPaddingLeft() + getPaddingRight();
+        int waveHeight;
+        if (heightMode == MeasureSpec.EXACTLY) {
+            waveHeight = heightSize - getPaddingBottom() - getPaddingTop();
+            waveHeight = Math.max(MIN_HEIGHT, waveHeight);
+        } else {
+            waveHeight = DEFAULT_HEIGHT;
+        }
+        heightSize = waveHeight + getPaddingTop() + getPaddingBottom();
+        widthMeasureSpec = MeasureSpec.makeMeasureSpec(widthSize, MeasureSpec.EXACTLY);
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(heightSize, MeasureSpec.EXACTLY);
+        setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    private int dp2px(float dpValue) {
+        float scale = getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    @Override
+    protected void onVisibilityChanged(@NonNull View changedView, int visibility) {
+        super.onVisibilityChanged(changedView, visibility);
+        if (visibility == View.VISIBLE) {
+            startAnim();
+        } else {
+            stopAnim();
+        }
+    }
+
+    @Override
+    public void invalidate() {
+        if (hasWindowFocus()) {
+            super.invalidate();
+            startAnim();
+        } else {
+            stopAnim();
         }
     }
 
@@ -163,14 +212,6 @@ public class WaveView extends LinearLayout {
         }
     }
 
-    /**
-     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
-     */
-    private int dp2px(float dpValue) {
-        float scale = getResources().getDisplayMetrics().density;
-        return (int) (dpValue * scale + 0.5f);
-    }
-
     @Override
     public void onWindowFocusChanged(boolean hasWindowFocus) {
         super.onWindowFocusChanged(hasWindowFocus);
@@ -179,56 +220,6 @@ public class WaveView extends LinearLayout {
             startAnim();
         } else {
             stopAnim();
-        }
-    }
-
-    @Override
-    protected void onVisibilityChanged(@NonNull View changedView, int visibility) {
-        super.onVisibilityChanged(changedView, visibility);
-        if (visibility == View.VISIBLE) {
-            startAnim();
-        } else {
-            stopAnim();
-        }
-    }
-
-    @Override
-    public void invalidate() {
-        if (hasWindowFocus()) {
-            super.invalidate();
-            startAnim();
-        } else {
-            stopAnim();
-        }
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
-        // 计算所需的宽度，忽略计算宽度很大，比方说屏幕宽度不足以完全显示控件的情况
-        int widthSize = mChildViews.length * mChildWidth + (mChildViews.length - 1) * mChildMargin
-                + getPaddingLeft() + getPaddingRight();
-        if (heightMode == MeasureSpec.EXACTLY) {
-            mChildHeight = heightSize - getPaddingBottom() - getPaddingTop();
-            mChildHeight = Math.max(MIN_HEIGHT, mChildHeight);
-        } else {
-            mChildHeight = DEFAULT_HEIGHT;
-        }
-        heightSize = mChildHeight + getPaddingTop() + getPaddingBottom();
-        widthMeasureSpec = MeasureSpec.makeMeasureSpec(widthSize, MeasureSpec.EXACTLY);
-        heightMeasureSpec = MeasureSpec.makeMeasureSpec(heightSize, MeasureSpec.EXACTLY);
-        setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
-        updateChildViewHeight();
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    }
-
-    private void updateChildViewHeight() {
-        for (View view : mChildViews) {
-            ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
-            layoutParams.height = mChildHeight;
-            view.setLayoutParams(layoutParams);
-            view.invalidate();
         }
     }
 }
